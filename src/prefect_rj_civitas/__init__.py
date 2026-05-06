db8d4f0538023f6f40e446fea09f0a48d0430d5d
nicolasevilasio <nicolas_evilasio@hotmail.com>
2026-05-06 17:53:18 -0300

    feat(flow_control): add skip_if_already_running utility

--- src/prefect_rj_civitas/__init__.py
+++ src/prefect_rj_civitas/__init__.py
@@ -5,10 +5,12 @@ from .env import (
 )
 from .config import config
 from .tasks.prefect_deployment import run_deployment_task
+from .tasks.flow_control import skip_if_already_running
 
 __all__ = [
     "get_pipeline_secrets_task",
     "verify_secrets_task",
     "config",
     "run_deployment_task",
+    "skip_if_already_running",
 ]
